f7249fba34695b797ff74d669a2747a14341e19e
stingleff <stingleff@Sam-Tingleffs-MacBook-Pro.local>
2011-09-13 09:28:47 -0700

    simple logging error handler

--- src/main/java/com/mtbaker/client/ErrorHandler.java
+++ src/main/java/com/mtbaker/client/ErrorHandler.java
@@ -2,5 +2,7 @@ package com.mtbaker.client;
 
 public interface ErrorHandler {
 
+	public void exception(String namespace, String key, Throwable error);
+
 	public void numberFormatException(String value, Class<? extends Number> cls);
 }
